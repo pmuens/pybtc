46e25bb74cb09b4c7ef8850ecf9cd2f4c9180f3b
Philipp Muens <raute1337@gmx.de>
2020-12-10 17:45:26 +0100

    Implement S256Field class

---
+++ ecc/S256Field.py
@@ -0,0 +1,11 @@
+from ecc.FieldElement import FieldElement
+
+P = 2 ** 256 - 2 ** 32 - 977
+
+
+class S256Field(FieldElement):
+    def __init__(self, num, prime=None):
+        super().__init__(num=num, prime=P)
+
+    def __repr__(self):
+        return "{:x}".format(self.num).zfill(64)
