02e400b56cbd0a448aba59a70ba7d4b953be8d81
Philipp Muens <raute1337@gmx.de>
2020-12-10 17:46:06 +0100

    Implement PrivateKey class

---
+++ ecc/PrivateKey.py
@@ -0,0 +1,53 @@
+import hmac
+import hashlib
+from random import randint
+from unittest import TestCase
+
+from ecc.S256Point import G, N
+from ecc.Signature import Signature
+
+
+class PrivateKey:
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret * G
+
+    def hex(self):
+        return "{:x}".format(self.secret).zfill(64)
+
+    def sign(self, z):
+        k = self.deterministic_k(z)
+        r = (k * G).x.num
+        k_inv = pow(k, N - 2, N)
+        s = (z + r * self.secret) * k_inv % N
+        if s > N / 2:
+            s = N - s
+        return Signature(r, s)
+
+    def deterministic_k(self, z):
+        k = b"\x00" * 32
+        v = b"\x01" * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, "big")
+        secret_bytes = self.secret.to_bytes(32, "big")
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b"\x00" + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b"\x01" + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, "big")
+            if candidate >= 1 and candidate < N:
+                return candidate
+            k = hmac.new(k, v + b"\x00", s256).digest()
+            v = hmac.new(k, v, s256).digest()
+
+
+class PrivateKeyTest(TestCase):
+    def test_sign(self):
+        pk = PrivateKey(randint(0, N))
+        z = randint(0, 2 ** 256)
+        sig = pk.sign(z)
+        self.assertTrue(pk.point.verify(z, sig))
